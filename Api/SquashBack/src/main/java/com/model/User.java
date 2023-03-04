2db66568a3af8b495d6c1e5da0c4e37b4c70e573
ejss4343 <jba4213@g.rit.edu>
2023-03-04 17:43:52 -0500

    add user class

---
+++ Api/SquashBack/src/main/java/com/model/User.java
@@ -0,0 +1,26 @@
+package com.model;
+
+public class User implements Comparable<User> {
+ 
+    private int id;
+    private String name;
+    private int squashes;
+
+    static final int DEF_SQUASH_CNT = 50;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.squashes = DEF_SQUASH_CNT;
+    }
+
+    @Override
+    public int compareTo(User other) {
+        return this.squashes - other.getSquashes();
+    }
+
+    public int getSquashes() {return squashes;}
+    public int getId() {return id;}
+    public String getName() {return name;}
+
+}
