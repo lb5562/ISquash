19c9ccf2812a30243d41cfb528f24a9053f82980
lb5562 <lucille_blain@live.com>
2023-03-04 16:59:05 -0500

    Fixed maven issue

--- Api/SquashBack/src/main/java/com/example/demo/DemoApplication.java
+++ Api/SquashBack/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.print("Hello I'm working HEELLLLLOOOOOO");
 		SpringApplication.run(DemoApplication.class, args);
+
 	}
 
 }
