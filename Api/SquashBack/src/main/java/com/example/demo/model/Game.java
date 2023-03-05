d5976e400c0cfd2f66c2dec438c42f6ee6bd1134
ejss4343 <jba4213@g.rit.edu>
2023-03-04 19:57:53 -0500

    add Game constructor with ball and status

--- Api/SquashBack/src/main/java/com/example/demo/model/Game.java
+++ Api/SquashBack/src/main/java/com/example/demo/model/Game.java
@@ -2,6 +2,12 @@ package com.example.demo.model;
 
 public class Game {
 
+    private Status status;
+    private Ball ball;
     
-    
+    public Game(Ball ball) {
+        this.status = Status.IN_PROGRESS;
+        this.ball = ball;
+    }
+
 }
