cd61e169888aaa6d6365b32cf42cb891dc54153b
ejss4343 <jba4213@g.rit.edu>
2023-03-05 07:24:59 -0500

    add Reactions and influences at 0.1, -0.1, and -0.2 for cheer, boo, and squash respectivly

---
+++ Api/SquashBack/src/main/java/com/example/demo/model/Boo.java
@@ -0,0 +1,11 @@
+package com.example.demo.model;
+
+public class Boo extends Reaction {
+
+    public static final double INFLUENCE = -0.1;
+
+    public Boo(){
+        super(INFLUENCE);
+    }
+
+}
